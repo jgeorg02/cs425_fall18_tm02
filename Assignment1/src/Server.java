812b5a1ac9b80a090ca716a4817f220b8eb29b5d
jgeorg02 <jgeorg02@cs.ucy.ac.cy>
2018-10-11 18:40:33 +0300

    Implemented Server and Server Thread Classes.

---
+++ Assignment1/src/Server.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.net.*;
+ 
+/**
+ * This program demonstrates a simple TCP/IP socket server that echoes every
+ * message from the client in reversed form.
+ * This server is multi-threaded.
+ */
+public class Server {
+ 
+    public static void main(String[] args) {
+ 
+        int port = 999;
+ 
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+ 
+            System.out.println("Server is listening on port " + port);
+ 
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("New client connected");
+ 
+                new ServerThread(socket).start();
+            }
+ 
+        } catch (IOException ex) {
+            System.out.println("Server exception: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ Assignment1/src/ServerThread.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.net.*;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+
+/**
+ * This thread is responsible to handle client connection.
+ */
+public class ServerThread extends Thread {
+	private Socket socket;
+
+	public ServerThread(Socket socket) {
+		this.socket = socket;
+	}
+
+	public void run() {
+		try {
+			InputStream input = socket.getInputStream();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+			OutputStream output = socket.getOutputStream();
+			PrintWriter writer = new PrintWriter(output, true);
+
+			String text;
+			text = reader.readLine();
+			System.out.println("Server: " + text);
+
+			String message[] = text.split(" ");
+
+			/*
+			 * Long time = new Long(System.currentTimeMillis()); ByteBuffer buffer =
+			 * ByteBuffer.allocate(Long.BYTES); buffer.putLong(time);
+			 * 
+			 * System.out.println(new String(buffer.array(), Charset.forName("UTF-8")));
+			 */
+
+			long l = System.currentTimeMillis();
+			byte b[] = new byte[8];
+
+			ByteBuffer buf = ByteBuffer.wrap(b);
+			buf.putLong(l);
+System.out.println(b.length);
+			//if (b.length <= 300 * 1000 && b.length >= 2000 * 1000) System.out.println("hi");
+
+				writer.println("WELCOME " + message[message.length - 1]);
+
+			socket.close();
+		} catch (IOException ex) {
+			System.out.println("Server exception: " + ex.getMessage());
+			ex.printStackTrace();
+		}
+	}
+}
