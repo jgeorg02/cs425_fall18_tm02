8133df087361cd4cb79a565133dccdd1864f4e37
stavroulakousparou <31859321+stavroulakousparou@users.noreply.github.com>
2018-10-22 00:05:02 +0300

    Add comments

--- Assignment1/src/ClientGenerator.java
+++ Assignment1/src/ClientGenerator.java
@@ -1,11 +1,21 @@
+/**
+ * This class is responsible to create a new Client. Every new Client need to
+ * know the IP address, port number, the number of users who send a request and
+ * the number of requests that each user send. These need to give as arguments.
+ * 
+ */
 
 public class ClientGenerator {
 
 	public static void main(String args[]) {
 
-		// default value
-		int N = 10, repetitions = 300;
+		// default value for number of users
+		int N = 10;
+		
+		// default value for number of requests
+		int repetitions = 300;
 
+		// checks the arguments
 		if (args.length == 0) {
 			System.out.println("You have to give server's ip and port! ");
 			return;
@@ -14,6 +24,7 @@ public class ClientGenerator {
 			repetitions = Integer.parseInt(args[3]);
 		}
 
+		// create new clients
 		for (int i = 1; i <= N; i++) {
 			new Client(i, args[0], Integer.parseInt(args[1]), repetitions).start();
 		}
