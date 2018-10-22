d22deaa0948d17beaf58fe8e86ac99f9fc82d51f
jgeorg02 <jgeorg02@cs.ucy.ac.cy>
2018-10-22 17:57:59 +0300

    Fixed payload size and added comments

--- Assignment1/src/Server.java
+++ Assignment1/src/Server.java
@@ -8,11 +8,17 @@ import javax.management.MBeanServer;
 import javax.management.ObjectName;
 
 /**
- * This program demonstrates a simple TCP/IP socket server that echoes every
- * message from the client in reversed form. This server is multi-threaded.
+ * This program demonstrates a simple TCP/IP socket server. This server is
+ * multi-threaded, it created the threads and lets them serve the client.
  */
 public class Server {
 
+	/**
+	 * This method is responsible for getting the cpu load.
+	 * 
+	 * @return the percentage value of the cpu load.
+	 * @throws Exception
+	 */
 	public static double getProcessCpuLoad() throws Exception {
 
 		MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
@@ -25,55 +31,57 @@ public class Server {
 		Attribute att = (Attribute) list.get(0);
 		Double value = (Double) att.getValue();
 
-		// usually takes a couple of seconds before we get real values
 		if (value == -1.0)
 			return Double.NaN;
-		// returns a percentage value with 1 decimal point precision
+
 		return ((int) (value * 1000) / 10.0);
 	}
 
 	public static void main(String[] args) {
 
-		int port = 9999;
-		int requests = 0;
-		long start;
+		int port = 9999, counter = 0, requests = 0; // the counter of the requests that the server served.
+		long start, now;
+		double averageCpu = 0, averageUtilizationMem = 0;
 
 		if (args.length == 1)
 			port = Integer.parseInt(args[0]);
 
 		// Calculate current memory use:
-		long beforeUsedMem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
-		long afterUsedMem;
-		long actualMemUsed;
+		long afterUsedMem = 0, actualMemUsed = 0,
+				beforeUsedMem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
 
 		try (ServerSocket serverSocket = new ServerSocket(port)) {
 
 			System.out.println("Server is listening on port " + port);
 
-			start = System.nanoTime();
+			start = System.nanoTime(); // the current time of starting the server.
 
 			while (true) {
 				Socket socket = serverSocket.accept();
 
-				requests++;
+				requests++; // counts the requests
+				counter++;
 				new ServerThread(socket).start();
 
-				if ((System.nanoTime() - start) >= 1000000) {
+				now = System.nanoTime(); // current time of getting a request.
+				
+				if ((now - start) >= 1000000) {
 
 					// Throughput
-					System.out.println("Requests for a millisec: " + requests);
+					System.out.println("Requests for " + ((now - start) / 1000000) + " millisec: " + requests);
 					requests = 0;
 					start = System.nanoTime();
 
 					// Average Memory Utilization
 					afterUsedMem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
 					actualMemUsed = Math.abs(afterUsedMem - beforeUsedMem);
-					System.out.println("Average Memory Utilization: " + actualMemUsed);
+					averageUtilizationMem += actualMemUsed;
+					System.out.println("Memory Utilization: " + averageUtilizationMem / counter);
 					beforeUsedMem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
 
 					// Average CPU Load
 					try {
-						System.out.println("Average CPU load: " + getProcessCpuLoad());
+						System.out.println("CPU load: " + getProcessCpuLoad());
 					} catch (Exception e) {
 						e.printStackTrace();
 					}
--- Assignment1/src/ServerThread.java
+++ Assignment1/src/ServerThread.java
@@ -2,47 +2,64 @@ import java.io.*;
 import java.net.*;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
+import java.util.Arrays;
 
 /**
- * This thread is responsible to handle client connection.
+ * This thread is responsible to handle client connection. Responds with a
+ * simple welcome user_id and send the payload size and the payload. The payload
+ * is generated with a random size.
  */
 public class ServerThread extends Thread {
-	private Socket socket;
+	private Socket socket; // the socket
+	private int min = 300 * 1024; // min payload size
+	private int max = 2000 * 1024; // max payload size
+	private int payloadSize = (int) (Math.random() * ((max - min) + 1)) + min;
 
+	/**
+	 * This is the constructor of this class. It initializes the socket that the
+	 * client and the server are connected.
+	 * 
+	 * @param socket
+	 */
 	public ServerThread(Socket socket) {
 		this.socket = socket;
 	}
 
+	/**
+	 * This function creates an array that will contain the payload. It fills it
+	 * with 1.
+	 * 
+	 * @return the payload
+	 */
+	public byte[] getPayload() {
+
+		byte[] payload = new byte[payloadSize];
+		Arrays.fill(payload, (byte) 1);
+		return payload;
+	}
+
+	/**
+	 * This function contains the functionality of this thread. It reads the
+	 * client's message and responds to it with a "Welcome" message. It also sends
+	 * the payload size and the payload.
+	 */
 	public void run() {
-		
+
 		try {
 			InputStream input = socket.getInputStream();
 			BufferedReader reader = new BufferedReader(new InputStreamReader(input));
 
-			OutputStream output = socket.getOutputStream();
-			PrintWriter writer = new PrintWriter(output, true);
+			DataOutputStream dataOutput = new DataOutputStream(socket.getOutputStream());
 
-			String text;
-			int min = 300;
-			int max = 2000;
-
-			text = reader.readLine();
+			String text = reader.readLine(); // the message that the client sent.
 
 			String message[] = text.split(" ");
-			String payload = "payload: ";
-		
-			int payloadSize = (int) (Math.random() * ((max - min) + 1)) + min;
-			payloadSize *= 1024;
 
-			writer.println("WELCOME " + message[message.length - 1] + " " + payloadSize);
-			writer.flush();
-			for (int i = 0; i < payloadSize; i++) 
-				payload += '*'; 
-		
-			writer.println(payload);
-			writer.flush();
-			
-			
+			dataOutput.writeUTF("WELCOME " + message[message.length - 1] + " " + payloadSize);
+
+			dataOutput.writeInt(payloadSize); // write length of the message
+			dataOutput.write(getPayload()); // write the message
+
 			socket.close();
 		} catch (IOException ex) {
 			System.out.println("Server exception: " + ex.getMessage());
